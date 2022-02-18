6fd466a590e6028bf47f9bc0863e409594680ce8
MrPink925 <cpinkster3@gmail.com>
2022-02-18 16:54:35 -0500

    added trent university

--- Scripts/ScrollAnimation.js
+++ Scripts/ScrollAnimation.js
@@ -1,7 +1,9 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
     window.sr = ScrollReveal({reset: true});
-    ScrollReveal().reveal('.nameTagLetter', {origin: 'bottom',distance: '2em', interval:125, duration: 1000 , easing: 'cubic-bezier(.17,1.46,.99,1.29)'});
+
+    ScrollReveal().reveal('.nameTagLetter', {origin: 'bottom',distance: '2em', interval:100, duration: 1000 , easing: 'cubic-bezier(.17,1.43,.99,1.27)'});
+    ScrollReveal().reveal('.nameTagLetterSmall', {origin: 'bottom',distance: '2em', interval:100, duration: 1000 , easing: 'cubic-bezier(.17,1.43,.99,1.27)', delay: 1300});
 
     ScrollReveal().reveal('.FullPane', { interval: 2000 }, { duration: 3000 });
     ScrollReveal().reveal('.LeftPane', { distance: '25%'},{ interval: 200 }, { duration: 3000 });
