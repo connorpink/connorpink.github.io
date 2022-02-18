2fbebfb7a272f32013f00e3a3bd10d14b80b02ed
MrPink925 <77735965+connorpink@users.noreply.github.com>
2022-02-18 16:01:19 -0500

    animation

--- Scripts/ScrollAnimation.js
+++ Scripts/ScrollAnimation.js
@@ -1,12 +1,13 @@
+
 document.addEventListener("DOMContentLoaded", function(event) {
     window.sr = ScrollReveal({reset: true});
-    ScrollReveal().reveal('#nameTag', { distance: '2em' },{ origin: 'top'}, { interval: 200 }, { duration: 2000 },{easing: 'cubic-bezier(0.5, 0, 0, 1)'});
-    ScrollReveal().reveal('#nameTag', { origin: 'top'});
-    ScrollReveal().reveal('.FullPane', { interval: 200 }, { duration: 3000 });
+    ScrollReveal().reveal('.nameTagLetter', {origin: 'bottom',distance: '2em', interval:125, duration: 1000 , easing: 'cubic-bezier(.17,1.46,.99,1.29)'});
+
+    ScrollReveal().reveal('.FullPane', { interval: 2000 }, { duration: 3000 });
     ScrollReveal().reveal('.LeftPane', { distance: '25%'},{ interval: 200 }, { duration: 3000 });
     ScrollReveal().reveal('.LeftPane', { origin: 'left'} );
-    ScrollReveal().reveal('.RightPane',{ distance: '25%'}, { delay: 200 }, { duration: 3000 });
-    ScrollReveal().reveal('.RightPane', { origin: 'right'} );
+    ScrollReveal().reveal('.RightPane',{ distance: '25%'}, { duration: 3000 });
+    ScrollReveal().reveal('.RightPane', { origin: 'left'} );
 
     ScrollReveal().reveal('.projectPane', {distance: '100%'}, { interval: 200 }, { duration: 4000 });
     ScrollReveal().reveal('.projectPane', { origin: 'left'} );
