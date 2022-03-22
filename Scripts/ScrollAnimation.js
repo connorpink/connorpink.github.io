c19f6cdb1b96815f0032a857a0f7fba9d579fd88
MrPink925 <77735965+connorpink@users.noreply.github.com>
2022-03-22 13:50:36 -0400

    disable slide animation on mobile

--- Scripts/ScrollAnimation.js
+++ Scripts/ScrollAnimation.js
@@ -5,12 +5,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
     ScrollReveal().reveal('.nameTagLetter', {origin: 'bottom',distance: '2em', interval:100, duration: 900 , easing: 'cubic-bezier(.17,1.43,.99,1.27)'});
     ScrollReveal().reveal('.nameTagLetterSmall', {origin: 'bottom',distance: '2em', duration: 1000 , easing: 'cubic-bezier(.17,1.43,.99,1.27)', delay: 1800});
 
-    ScrollReveal().reveal('.LeftPane', { distance: '25%'},{ interval: 200 }, { duration: 3000 });
+    ScrollReveal().reveal('.LeftPane', { distance: '25%', interval: 200, mobile: false });
     ScrollReveal().reveal('.LeftPane', { origin: 'left'} );
-    ScrollReveal().reveal('.RightPane',{ distance: '25%'}, { duration: 3000 });
+    ScrollReveal().reveal('.RightPane',{ distance: '25%', interval: 200, mobile: false });
     ScrollReveal().reveal('.RightPane', { origin: 'left'} );
 
-    ScrollReveal().reveal('.projectPane', {distance: '100%'}, { interval: 200 }, { duration: 4000 });
+    ScrollReveal().reveal('.projectPane', {distance: '100%', interval: 200, mobile: false });
     ScrollReveal().reveal('.projectPane', { origin: 'left'} );
 
 });
